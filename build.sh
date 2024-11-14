bdcfc742953a4e98a3056e041d53576c9cbbcb4b
Skye Chappelle <skyeforeverblue@outlook.com>
2024-11-14 02:49:48 -0500

    Edit package list

--- build.sh
+++ build.sh
@@ -13,11 +13,15 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+# rpm-ostree install screen
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
+
+
+# Install Fira fonts
+rpm-ostree install mozilla-fira-sans-fonts fira-code-fonts
